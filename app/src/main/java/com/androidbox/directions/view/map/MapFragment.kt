d077c0726e765dda0f5d3b93a0a5eef6b954199a
Sergio81 <sergio.aguilar@mobileappscompany.us>
2019-03-01 23:11:25 -0600

    Add google map

--- app/src/main/java/com/androidbox/directions/view/map/MapFragment.kt
+++ app/src/main/java/com/androidbox/directions/view/map/MapFragment.kt
@@ -8,13 +8,83 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 import com.androidbox.directions.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.fragment_map.*
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+    private var markerCity: Marker? = null
+    private var mMap: GoogleMap? = null
+    private var isMapReady = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_map, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.onCreate(null)
+        mapView.getMapAsync(this)
+    }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        isMapReady = true
+        showCity()
+    }
+
+    private fun showCity() {
+        val cityLatLng = LatLng(40.730610, -73.935242)
+
+        if (markerCity != null)
+            markerCity!!.remove()
+
+        mMap!!.uiSettings.isZoomControlsEnabled = true
+        markerCity = mMap!!.addMarker(MarkerOptions().position(cityLatLng))
+
+        val cameraPosition = CameraPosition.Builder()
+            .target(cityLatLng)      // Sets the center of the map to the selected city
+            .zoom(10f)                   // Sets the zoom to view City Level
+            .bearing(20f)                // Sets the orientation of the camera to east
+            .tilt(30f)                   // Sets the tilt of the camera to 30 degrees
+            .build()                   // Creates a CameraPosition from the builder
+
+        mMap!!.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onPause() {
+        mapView.onPause()
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        mapView.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
 }
